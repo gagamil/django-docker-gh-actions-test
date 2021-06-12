847bc0d0f9f0e34dfafb9b362c708154cbbf1f7f
Gabriel Gamil <gamil.gabriel@gmail.com>
2021-06-12 22:24:06 +0300

    Use allowed hosts dat afrom ENV

--- dckrdjnggthb/settings.py
+++ dckrdjnggthb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-fqd*v=+6)-i8ssbzi930k5+ot4@8po05=71!8&%wh!=a&dfg%k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
